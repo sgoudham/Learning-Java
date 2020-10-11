ccc6bed7b9de567223996a1419c9e60ce4bf5978
sgoudham <sgoudham@gmail.com>
2020-10-11 01:02:53 +0100

    Demonstrating Custom Interfaces

---
+++ Java Classes & Interfaces/New Calc Engine/src/MathProcessing.java
@@ -0,0 +1,9 @@
+public interface MathProcessing {
+    
+    String SEPARATOR = " ";
+    String getKeyWord();
+    double doCalculation(double leftVal, double rightVal);
+    default String doFormatting() {
+        return null;
+    }
+}

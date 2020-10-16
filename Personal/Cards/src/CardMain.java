b5ac64f8f979f7818ea45ed1ea293c27789c7db9
sgoudham <sgoudham@gmail.com>
2020-10-16 23:27:28 +0100

    Main Method For Card Games

---
+++ Personal/Cards/src/CardMain.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class CardMain {
+    public static void main(String[] args) {
+        /* Play Sevens */
+        Sevens sevens = new Sevens();
+        sevens.shuffle();
+        List<Player> allPlayers = sevens.dealCards(4);
+        sevens.play(allPlayers, 3);
+
+    }
+}

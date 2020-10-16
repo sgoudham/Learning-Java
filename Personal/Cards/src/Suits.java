f95103221af7cd10cf30c857173e518b4f0b5dfe
sgoudham <sgoudham@gmail.com>
2020-10-16 23:28:01 +0100

    Enums for Suits/Faces

---
+++ Personal/Cards/src/Suits.java
@@ -0,0 +1,13 @@
+public enum Suits {
+    SPADES("Spades"), HEARTS("Hearts"), DIAMONDS("Diamonds"), CLUBS("Clubs");
+
+    private String suit;
+
+    Suits(String suit) {
+        this.suit = suit;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+}

5a01203177e487c031780a352e9934c49e07b245
sgoudham <sgoudham@gmail.com>
2020-10-19 23:06:31 +0100

    User can now enter in the number of players

--- Personal/Cards/src/CardMain.java
+++ Personal/Cards/src/CardMain.java
@@ -1,12 +1,26 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class CardMain {
     public static void main(String[] args) {
         /* Play Sevens */
         Sevens sevens = new Sevens();
         sevens.shuffle();
-        List<Player> allPlayers = sevens.dealCards(4);
-        sevens.play(allPlayers, 4);
 
+        int numOfPlayers = getNumOfPlayers();
+        List<Player> allPlayers = sevens.dealCards(numOfPlayers);
+
+        sevens.play(allPlayers, numOfPlayers);
+
+    }
+
+    public static int getNumOfPlayers() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please Enter The Number of Players!");
+        while (!in.hasNextInt()) {
+            in.nextLine();
+            System.out.println("Invalid Amount of Players! Try again!");
+        }
+        return in.nextInt();
     }
 }

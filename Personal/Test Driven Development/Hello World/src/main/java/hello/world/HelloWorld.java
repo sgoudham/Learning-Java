9f9a3f68d4a6cebe7e33f7ad7cda4b2a7a6ccbdc
Hammy <sgoudham@gmail.com>
2020-11-05 22:30:32 +0000

    Add Hello World Project With TDD Frameworks

---
+++ Personal/Test Driven Development/Hello World/src/main/java/hello/world/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello.world;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeting greeting = new Greeting("Hello World");
+        System.out.println(greeting.getMessage());
+    }
+}
---
+++ Personal/Test Driven Development/Hello World/src/test/java/hello/world/HelloWorldTest.java
@@ -0,0 +1,25 @@
+package hello.world;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HelloWorldTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void greetingMessageShouldPass() {
+        Greeting greeting = new Greeting("Hello World");
+        assertEquals("Hello World", greeting.getMessage());
+    }
+
+    @Test
+    public void greetingMessageShouldFail() {
+        Greeting greeting = new Greeting("Not Hello World");
+        assertEquals("Hello World", greeting.getMessage());
+    }
+}

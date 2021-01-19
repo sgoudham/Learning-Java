95422528ac8aa549875c5523de983112afc3019f
sgoudham <sgoudham@gmail.com>
2021-01-19 12:26:25 +0000

    Explicitly define Bean Scope

--- Spring/src/main/java/conference/AppConfig.java
+++ Spring/src/main/java/conference/AppConfig.java
@@ -4,14 +4,17 @@ import conference.repository.HibernateSpeakerRepositoryImpl;
 import conference.repository.SpeakerRepository;
 import conference.service.SpeakerService;
 import conference.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 
 public class AppConfig {
 
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         return new SpeakerServiceImpl(getSpeakerRepository());
     }
